9b62d3b4ff9df436da3ed06bf4c9a7f4443255aa
sachit <sachitforsocials@gmail.com>
2024-09-03 13:43:43 +0530

    change in TodoForm.jsx and TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import { useTodo } from "../context";
+
 function TodoItem({ todo }) {
     
+    const [isTodoEditable ,setIsTodoEditable] = useState(false)
+    const [todoMsg , setTodoMsg] = useState(todo.todo)
+
+    const {updateTodo,deleteTodo,checkTodo} = useTodo()
+
+    const editTodo = () => {
+        updateTodo(todo.id,{...todo, todo:todoMsg})
+    }
+
+    const checkTodoComplete = () =>{
+        checkTodo(todo.id)
+    }
+
 
     return (
         <div
